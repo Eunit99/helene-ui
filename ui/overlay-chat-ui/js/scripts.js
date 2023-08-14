53f5f2ca331c81ca53cb3b1b5d5a83ab008956c5
Ξunit <eunithelp@gmail.com>
2023-08-14 10:04:25 +0100

    Fix: Fix chatbot hiding when body is clicked

--- ui/overlay-chat-ui/js/scripts.js
+++ ui/overlay-chat-ui/js/scripts.js
@@ -1,39 +1,61 @@
-// Script for Chat window START
-$("body").ready(function () {
-	$(".chatbox_container").hide().delay(100).fadeIn(1000); // You can customize the time you want the chat window to be displayed with the 'delay' and 'fadeIn' properties
-	$(".chatbox_container").removeClass("displayNoneChat"); //Removes class "displayNoneChat" and displays the Chatbox container
-});
-// Script for Chat window END
-
-// Script for Chat window START
 (function ($) {
-	$(document).ready(function () {
-		var $chatbox = $(".chatbox"),
-			$chatboxTitle = $(".chatbox__title"),
-			$chatboxTitleClose = $(".chatbox__title__close"),
-			$chatboxCredentials = $(".chatbox__credentials");
-		$("#instantChat").on("click", function () {
-			$chatbox.removeClass("chatbox--closed");
-			$chatbox.toggleClass("chatbox--tray");
-		});
-		$chatboxTitleClose.on("click", function (e) {
-			e.stopPropagation();
-			$chatbox.addClass("chatbox--closed");
-		});
-		$chatbox.on("transitionend", function () {
-			if ($chatbox.hasClass("chatbox--closed")) $chatbox.remove();
-		});
-	});
+  $(document).ready(function () {
+    var $chatbox = $(".chatbox"),
+      $chatboxTitleClose = $(".chatbox__title__close"),
+      $chatbox_container = $(".chatbox_container"),
+      $floatingBubble = $(".floatingBubble"),
+      $chatbox__title = $(".chatbox__title");
+
+    $("body").ready(function () {
+      $(".chatbox_container").hide().delay(1000).fadeIn(1000); // You can customize the time you want the chat window to be displayed with the 'delay' and 'fadeIn' properties
+      $(".chatbox_container").removeClass("displayNoneChat"); //Removes class "displayNoneChat" and displays the Chatbox container
+      $floatingBubble.hide(); // Hide the floating bubble by default
+    });
+    $chatbox__title.on("click", function () {
+      $floatingBubble.fadeOut(100).hide(); // fadeOut (100ms) of the floatingBubble
+      $chatbox_container.removeClass("displayNoneChat"); // Removes .displayNoneChat to display the Chatbox container
+      $chatbox.removeClass("chatbox--closed");
+      $chatbox.toggleClass("chatbox--tray");
+    });
+    $(".floatingBubble").on("click", function () {
+      $chatbox_container.removeClass("displayNoneChat");
+      $chatbox.removeClass("chatbox--closed");
+      $chatbox.toggleClass("chatbox--tray");
+      $floatingBubble.fadeOut(1e3).hide(); // fadeOut (100ms) of the floatingBubble
+    });
+    $(".closeChatWindow").on("click", function () {
+      // Closes the chat window once anywhere on the page is clicked apart from footer
+      $chatbox.addClass("chatbox--tray");
+    });
+    $chatboxTitleClose.on("click", function (e) {
+      e.stopPropagation();
+      $chatbox.addClass("chatbox--closed");
+      $(".floatingBubble")
+        .hide()
+        .fadeIn(1e3); // fadeIn (100ms) of the floatingBubble
+    });
+  });
 })(jQuery);
-// Script for Chat window END
+// Chat window manipulation END
 
 
 // Script for overlay START
 function openOverlay() {
-	document.getElementById("overlay").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
 }
 
 function closeOverlay() {
-	document.getElementById("overlay").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
 }
 //   Script for overlay END
+
+$(document).ready((function () {
+  var date = new Date,
+    sec = date.getSeconds(),
+    min = date.getMinutes(),
+    hour = date.getHours(),
+    copyrightYear = date.getFullYear();
+  $("document").ready((function () {
+    $("#copyright").text(copyrightYear)
+  }))
+}))
